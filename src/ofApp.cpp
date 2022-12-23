2bf730ab7b29dfb17e7fbf14799ac1d98757c708
Roberto Prince <roberto.prince@uttn.mx>
2022-12-22 21:02:25 -0600

    Move everything to mouse events to make a primitive drawing app.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -35,34 +35,7 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	ofBackground(ofColor::black);
-	
-	//Draw Rectangles from the center
-	ofSetRectMode(OF_RECTMODE_CENTER);
 
-	// Draw a rectangle if color blue
-	ofSetColor(0,0, 255);					// Set color to red
-	ofSetLineWidth(1);						// The line to 1px if any
-	ofFill();								// Make it to draw filled
-	ofDrawRectangle(200, 150, 150, 150);	// Draw the actual rect
-	// end
-
-	// Draw the outline of a Rectangle in green
-	ofSetColor(0, 255, 0);					// Set the color to green
-	ofNoFill();								// Set to just draw the outline
-	ofSetLineWidth(3);						// Make the outline to 3px
-	ofDrawRectangle(200, 150, 150, 150);	// Draw the rect outline on top of the other
-	// end
-
-
-	ofSetColor(255, 255, 255);
-	image.drawSubsection(200, 150, 75, 75, 0, 0, 64, 64);
-
-	// Draw a red semi-transparent Ellipse at the mouse ubication
-	ofSetLineWidth(1);
-	ofSetColor(255, 0, 0, 175);
-	ofFill();
-	ofDrawEllipse(mouseX, mouseY, 100, 100);
 	
 }
 
@@ -78,7 +51,11 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+	// Draw a red semi-transparent Ellipse at the mouse ubication
+	ofSetLineWidth(1);
+	ofSetColor(255, 0, 0, 50);
+	ofFill();
+	ofDrawEllipse(mouseX, mouseY, 10, 10);
 }
 
 //--------------------------------------------------------------
@@ -88,7 +65,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+	ofBackground(ofColor::black);
 }
 
 //--------------------------------------------------------------
